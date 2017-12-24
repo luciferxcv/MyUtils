fa0913fab47ff1080cde9e6ce5562568541c27cc
luciferxcv <miaodi1985@126.com>
2017-12-25 00:19:32 +0800

    first version

--- tinpotGrid/scripts/tinpotGrid.js
+++ tinpotGrid/scripts/tinpotGrid.js
@@ -1,13 +1,29 @@
 (function($){
     'use strict';
+    let dom;
     let option = {
         showRowNum: false,
         showPager: true,
         maxRows: 10
     };
+
     if(typeof $.fn.tpGrid == "undefined"){
-        $.fn.tpGrid = opt => {for(var attr in opt){option[attr] = opt[attr]};console.log(option)};
+        $.fn.tpGrid = tpGrid;
     }
 
+    function reload(){
+        console.log('reload');
+    }
 
+    function tpGrid(opt){
+        dom = this;
+        if(typeof opt == "string")
+            eval(opt + "()");
+        else{
+            for(let attr in opt){
+                option[attr] = opt[attr]
+            }
+            console.log(option);
+        }
+    }
 }(jQuery))
