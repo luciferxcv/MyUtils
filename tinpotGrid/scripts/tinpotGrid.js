77e31228600b14266cb8dba6f8dcb71322609286
luciferxcv <miaodi1985@126.com>
2017-12-24 23:18:55 +0800

    first version

--- tinpotGrid/scripts/tinpotGrid.js
+++ tinpotGrid/scripts/tinpotGrid.js
@@ -1,9 +1,13 @@
 (function($){
     'use strict';
-    if(typeof tpGrid == "undefined"){
-        $.fn.tpGrid = tpGrid;
+    let option = {
+        showRowNum: false,
+        showPager: true,
+        maxRows: 10
+    };
+    if(typeof $.fn.tpGrid == "undefined"){
+        $.fn.tpGrid = opt => {for(var attr in opt){option[attr] = opt[attr]};console.log(option)};
     }
-
 
 
 }(jQuery))
